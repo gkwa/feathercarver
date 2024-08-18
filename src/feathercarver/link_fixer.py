f3e1aa021b603b0eb9bf738f766cba5ecfe27ce6
Taylor Monacelli <taylormonacelli@gmail.com>
2024-08-18 14:43:51 -0700

    logging

--- src/feathercarver/link_fixer.py
+++ src/feathercarver/link_fixer.py
@@ -56,6 +56,7 @@ class LinkFixer:
 
         for i in range(len(blocks)):
             if not self.block_pattern.match(blocks[i]):
+                self.logger.debug(blocks[i])
                 blocks[i] = self.pattern.sub(self._replace_link, blocks[i])
 
         return "".join(blocks)
